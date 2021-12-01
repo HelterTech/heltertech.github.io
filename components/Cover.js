9e4435d42ef04a7ce3a4934370e342d8601a2747
sharjeelyunus <sharjeel924@gmail.com>
2021-12-02 02:43:41 +0500

    📦 NEW: Cover

---
+++ components/Cover.js
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import CoverImage from '../assets/team-working.png';
+import styles from './components.module.css';
+
+export default function Cover() {
+    return (
+        <div className={styles.cover}>
+            <div className={styles.coverHeader}>
+                <p className={styles.coverHeader_heading}>Connecting People <br /> and Technology</p>
+                <a href="#footer-form" className={styles.coverHeader_btn}>Let's Talk</a>
+            </div>
+            <div className={styles.coverImage}>
+                <Image src={CoverImage} alt="" />
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import Cover from "../components/Cover";
+import CoverWhatWeDo from "../components/WhatWeDo";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Cover />
+      <CoverWhatWeDo />
     </div>
   )
 }
