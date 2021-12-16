4b36368cd5d364b03034e69c604fe65cc96fc94c
sharjeelyunus <sharjeel924@gmail.com>
2021-12-16 17:16:26 +0500

    👌 IMPROVE: What We Do (Page)

--- pages/what-we-do.js
+++ pages/what-we-do.js
@@ -9,10 +9,17 @@ import coronaTracker from '../assets/Corona tracker.jpg';
 import neon from '../assets/neon.jpg';
 import androidHelter from '../assets/androidhelter.jpg';
 import graphicsHelter from '../assets/graphicshelter.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserShield, faRocket, faCalculator } from '@fortawesome/free-solid-svg-icons';
+import Head from 'next/head';
 
 export default function whatWeDo() {
     return (
         <div>
+            <Head>
+                <title>Helter Tech | What We Do</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             <Navbar />
             <div className={styles.whatWeDo}>
                 <div className={styles.wwd_services}>
@@ -79,28 +86,33 @@ export default function whatWeDo() {
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div>
-                        <h1>WHY GO Helter Tech?</h1>
+                <div className={styles.why_services}>
+                    <div className={styles.why_header}>
+                        <h1>WHY GO HELTER TECH?</h1>
                         <p>We are dedicated to game-changing results.</p>
                     </div>
-                    <div>
-                        <h2>We Here For You</h2>
-                        <p>
-                            The Helter Tech is smart, humble, and driven to serve. Our clients consistently rank our development resources and project delivery as outstanding.
-                        </p>
-                    </div>
-                    <div>
-                        <h2>Leading Technology</h2>
-                        <p>
-                            Don’t get stuck behind the curve with an outdated stack. We are Helter for a reason - our experts live on the cutting edge to ensure the performance, security, cost, and scalability of your app is a grade above the competition
-                        </p>
-                    </div>
-                    <div>
-                        <h2>100% Accountability</h2>
-                        <p>
-                            Our step-by-step formula for reducing project risk, ensures your project will be delivered on time and on budget. Regardless of the obstacles, we pledge to deliver your project on schedule and exceed your expectations.
-                        </p>
+                    <div className={styles.why_details}>
+                        <div className={styles.why_service}>
+                            <FontAwesomeIcon icon={faUserShield} className={styles.serviceIcon} />
+                            <h2>We Here For You</h2>
+                            <p>
+                                The Helter Tech is smart, humble, and driven to serve. Our clients consistently rank our development resources and project delivery as outstanding.
+                            </p>
+                        </div>
+                        <div className={styles.why_service}>
+                            <FontAwesomeIcon icon={faRocket} className={styles.serviceIcon} />
+                            <h2>Leading Technology</h2>
+                            <p>
+                                Don’t get stuck behind the curve with an outdated stack. We are Helter for a reason - our experts live on the cutting edge to ensure the performance, security, cost, and scalability of your app is a grade above the competition
+                            </p>
+                        </div>
+                        <div className={styles.why_service}>
+                            <FontAwesomeIcon icon={faCalculator} className={styles.serviceIcon} />
+                            <h2>100% Accountability</h2>
+                            <p>
+                                Our step-by-step formula for reducing project risk, ensures your project will be delivered on time and on budget. Regardless of the obstacles, we pledge to deliver your project on schedule and exceed your expectations.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
