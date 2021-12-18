ae545e7bdc3f63f4dd3bf613e185ef16f815bd1a
sharjeelyunus <sharjeel924@gmail.com>
2021-12-18 19:18:17 +0500

    📦 NEW: Contact (Page)

---
+++ pages/contact.js
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+export default function contact() {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <h1>CONTACT US</h1>
+                <p>Get in touch to get the ball rolling.</p>
+            </div>
+            <Footer />
+        </div>
+    )
+}
